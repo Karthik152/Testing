e77e12edb4e52980e9966a9bf0cfcfbdd506bf42
Karthik <110925398+Karthik152@users.noreply.github.com>
2023-08-08 18:32:46 +0530

    Initial commit

---
+++ app/src/main/java/com/example/testing/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.testing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.mylibrary.Toaster;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toaster.simpleToast(this,"welcome..................!");
+    }
+}
